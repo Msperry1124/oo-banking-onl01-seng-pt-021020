d3f025acc8804eb240e84b72d3c27159020e025d
Melissa Sperry <sperry.mel@gmail.com>
2020-03-14 13:39:55 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,56 @@
 class BankAccount
+  attr_reader :name
+  attr_accessor :balance, :status
+
+  def initialize(name, balance = 1000, status = "open")
+    @name = name
+    @balance = balance
+    @status = status
+  end
+
+  def deposit(deposit_amount)
+    @balance += deposit_amount
+  end
+
+  def display_balance
+    "Your balance is $#{self.balance}."
+  end
+
+  def valid?
+    self.status == "open" && self.balance > 0 ? true : false
+  end
+
+  def close_account
+    self.status = "closed"
+  end
 
 end
+
+class Transfer
+  attr_accessor :sender, :receiver, :amount, :status
+  def initialize(sender, receiver, status = "pending", amount)
+    @sender = sender
+    @receiver = receiver
+    @status = status
+    @amount = amount
+  end
+  def valid?
+    sender.valid? && receiver.valid?
+  end
+  def execute_transaction
+    if valid? && sender.balance > amount && @status == "pending"
+      sender.balance -= amount
+      receiver.balance += amount
+      @status = "complete"
+    else
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+    end
+  end
+  def reverse_transfer
+    if @status == "complete"
+      @sender.balance += @amount
+      @receiver.balance -= @amount
+      @status = "reversed"
+    end
+  end
